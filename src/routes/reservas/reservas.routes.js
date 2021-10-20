95703f5231f8ffa7d78834818ce7eb786c9e37e8
SergioRodriguez317 <sergioarg17@gmail.com>
2021-10-19 19:53:08 -0500

    Form y post de reserva
    Se quita etiqueta form que sobra y se realiza el post de la reserva en la base de datos

--- src/routes/reservas/reservas.routes.js
+++ src/routes/reservas/reservas.routes.js
@@ -15,5 +15,10 @@ routes.get("/reservas-tickets", (req, res) => {
     res.render("reservas/reservas-tickets", { message });
   });
 
+routes.post("/reserva-tickets", async(req,res)=>{
+    const reserva = new _reservaSchema(req.body)
+    await reserva.save()
+    res.redirect("/reservas-tickets")
+})
 
 module.exports = routes;
