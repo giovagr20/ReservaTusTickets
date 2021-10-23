50553d0d340f6c5ad207968b34f79472fe899a92
alex3daah <alex3daah@gmail.com>
2021-10-23 09:47:44 -0500

    pagina principal se mejora colores y tamaño fotos

--- src/public/scriptini.js
+++ src/public/scriptini.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&&language=es-MX&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&language=es-MX&query="'
 
@@ -7,12 +7,12 @@ const form = document.getElementById('form')
 const search = document.getElementById('search')
 
 // Get initial movies
-//getMovies(API_URL)
+getMovies(API_URL)
 
 async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
-console.log(data)
+
     showMovies(data.results)
 }
 
@@ -22,12 +22,10 @@ function showMovies(movies) {
     movies.forEach((movie) => {
         const { title, poster_path, vote_average, overview } = movie
 
-        //console.log(movie)
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
 
         movieEl.innerHTML = `
-
             <img src="${IMG_PATH + poster_path}" alt="${title}">
             <div class="movie-info">
           <h3>${title}</h3>
@@ -38,17 +36,8 @@ function showMovies(movies) {
           ${overview}
         </div>
         `
-        movieEl.addEventListener("click", (e)=>{
-        
-            let movieSelect
-            movieSelect = movie
-            console.log (movieSelect)
-        return movieSelect
-       })
         main.appendChild(movieEl)
-
     })
- 
 }
 
 function getClassByRate(vote) {
@@ -61,8 +50,7 @@ function getClassByRate(vote) {
     }
 }
 
-form.addEventListener('keypress', (e) => {
-   if (e.key==='Enter'){
+form.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const searchTerm = search.value
@@ -73,11 +61,5 @@ form.addEventListener('keypress', (e) => {
         search.value = ''
     } else {
         window.location.reload()
-    }}
-    else
-    {
-        console.log("No ingresó datos")
     }
-    
-    
 })
