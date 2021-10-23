56dfc77bf6fe229164241dfa4b3bc1b1b6314c4f
alex3daah <alex3daah@gmail.com>
2021-10-23 12:19:37 -0500

    paginas informacion y se completa bd dispo

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const PORT = require('./database/utils/properties').PORT;
 const _comprasRoutes = require('./routes/compras/compras.routes');
 const _reservasRoutes = require('./routes/reservas/reservas.routes');
 const _disponibilidadesRoutes = require("./routes/disponibilidades/index");
+const legal = require("./routes/legal/index");
+const nosotros = require("./routes/nosotros/index");
+const contacto = require("./routes/contacto/index");
 DB();
 
 //Settings
@@ -21,6 +24,9 @@ app.use(express.static(__dirname + "/public"))
 app.use('/', _comprasRoutes);
 app.use('/', _reservasRoutes);
 app.use('/', _disponibilidadesRoutes);
+app.use('/', legal);
+app.use('/', nosotros);
+app.use('/', contacto);
 
 //RUN Server
 app.listen(app.get('port'), () => {
